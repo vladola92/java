4b6183f58ba5b54e453d34b68fe62770184c3c15
Ola <vladola92@github.com>
2019-02-12 23:43:02 +0200

    Tema 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,45 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(1+3);
+        // 1.Write a Java program to print 'Hello' on screen and then print your
+        //name on a separate line;
+
+        System.out.println("Hello");
+
+
+        // 2.Write a Java program to print ‘The addition of 7 and 8 is: ‘, and then the
+        //sum of the two numbers;
+
+        int x = 7;
+        int y = 8;
+
+        int scadere = x - y;
+
+        System.out.println("The addition of  and 8 is = " + scadere );
+
+        double impartire = x/y ;
+        System.out.println(impartire);  // aici inca nu inteleg de ce am rezultatul 0.0
+
+        double impartirea2 = 1.5/78;
+        System.out.println("Aceastea este adoua inercare de a impartii doua numere = " +  impartirea2);
+
+        //4.Write a Java program to print the result of the following operations:
+        //a. -5 + 8 * 6
+        //b. (55+9) % 9
+        //c. 20 + -3*5 / 8
+        //d. 5 + 15 / 3 * 2 - 8 % 3
+        System.out.println("puncutl a este = " + (-5 + 8 * 6));
+        System.out.println("puncutl b este = " + ((55+9) % 9));
+        System.out.println("puncutl c este = " + (20 + -3*5 / 8));
+        System.out.println("puncutl d este = " + (5 + 15 / 3 * 2 - 8 % 3));
+
+
+
+
+
+
+
+
+
     }
 }
